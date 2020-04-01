4ec2b4e41fcbdf021956f7a7c6905c2ec4b21c54
Tmat12 <tyler.mathews12@hotmail.com>
2020-04-01 14:25:15 -0500

    Added Base Models. Will continue to update them.

--- b2b/settings.py
+++ b2b/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'b2b_app.apps.B2BAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- b2b_app/models.py
+++ b2b_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=250)
+    contactEmail = models.EmailField(max_length = 250)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length = 250)
+    email = models.EmailField(max_length = 250)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    queries = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.name
