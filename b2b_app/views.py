f32584440e0b9d04408c001a1894f328f29b82f1
brennanvandyke <37941104+brennanvandyke@users.noreply.github.com>
2020-04-08 14:43:51 -0500

    Started the view

--- b2b_app/views.py
+++ b2b_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from checkmate.book_data import bookData
 
 # Create your views here.
+
+def searchView(request):
+
+    if 'searchJSON' in request.GET:
+        searchJSON = request.GET['searchJSON']
+    elif ('searchTitle' in request.GET) or ('searchAuthor' in request.GET) or ('searchISBN' in request.GET):
+        book_data = bookData()
+        if 'searchTitle' in request.GET:
+            book_data.title = request.GET['searchTitle']
+        if 'searchAuthor' in request.GET:
+            book_data.author = request.GET['searchAuthor']
+        if 'searchISBN' in request.GET:
+            book_data.ISBN = request.GET['searchISBN']
+
+        
+
