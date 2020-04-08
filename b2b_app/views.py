498cf868352ae0f3891af89bb2aafd49f65aa9f0
Tmat12 <tyler.mathews12@hotmail.com>
2020-04-08 14:59:43 -0500

    base view for company_list created

--- b2b_app/views.py
+++ b2b_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
+from django.views.generic import TemplateView, ListView
+
+from .models import Company, User, Query
 # Create your views here.
+
+class CompanyListView(ListView):
+    model = Company
+    template_name = "company_list_view.html"
+    company_list = Company.objects.all()
+    paginate_by = 5
+
+    def get_companies(self):
+        context = {
+            'company_list': company_list,
+        }
+        return context
