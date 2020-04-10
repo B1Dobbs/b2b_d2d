a6ba92639d8cc08e5fd9f003d5f0c8d73ecb7c38
Brooke Dobbins <brooke.dobbins@eagles.oc.edu>
2020-04-10 17:53:53 -0500

    User views done, bug with validation

--- b2b_app/urls.py
+++ b2b_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     #path('company/create', views.CompanyCreateView.as_view(), name='create_company'),
     #path('company/update/<int:pk>', views.CompanyUpdateView.as_view(), name='update_company'),
     #path('company/delete/<int:pk>', views.CompanyDeleteView.as_view(), name='delete_book'),
-    path('user/create', views.UserCreateView.as_view(), name='create_user'),
-    path('user/update/<int:pk>', views.UserUpdateView.as_view(), name='update_user'),
-    #path('user/delete/<int:pk>', views.UserDeleteView.as_view(), name='delete_user'),
+    path('company/<int:company_id>/user/create', views.UserCreateView.as_view(), name='create_user'),
+    path('company/<int:company_id>user/update/<int:pk>', views.UserUpdateView.as_view(), name='update_user'),
+    path('company/<int:company_id>/user/delete/<int:pk>', views.UserDeleteView.as_view(), name='delete_user'),
 ]
--- b2b_app/views.py
+++ b2b_app/views.py
@@ -11,6 +11,8 @@ from bootstrap_modal_forms.generic import (BSModalLoginView,
                                            BSModalDeleteView)
 from django.urls import reverse_lazy
 from .forms import CompanyForm, UserForm, UserChangeForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 class CompanyListView(ListView):
     model = Company
@@ -20,44 +22,69 @@ class CompanyListView(ListView):
 # Create your views here.
 class CompanyDetailView(DetailView):
     model = Company
-    template = loader.get_template('company/company_detail.html')
+    template = loader.get_template('company_detail.html')
     
     def get(self, request, *args, **kwargs):
-        pk =kwargs['pk']
         company = get_object_or_404(Company, pk=kwargs['pk'])
-        users = User.objects.filter(company = pk)
+        users = User.objects.filter(company = kwargs['pk'])
         context = {'company': company, 'users' : users}
         return render(request, 'company_detail.html', context)
 
-# #TODO
-# class CompanyCreateView(DetailView):
-    
-#     template = loader.get_template('company_detail.html')
-    
-#     def get(self, request, *args, **kwargs):
-#         pk =kwargs['pk']
-#         company = get_object_or_404(Company, pk=kwargs['pk'])
-#         users = User.objects.filter(company = pk)
-#         context = {'company': company, 'users' : users}
-#         return render(request, 'company_detail.html', context)
-
-
 class UserCreateView(BSModalCreateView):
-    print("CreateUser")
     form_class = UserForm
     template_name = 'user/create_user.html'
     success_message = 'Success: User was created.'
-    success_url = reverse_lazy('company_detail')
 
     def post(self, request, **kwargs):
-        request.POST = request.POST.copy()
-        request.POST['company_id'] = 1
+        company_id = self.kwargs['company_id']
+        self.success_url = reverse('company_detail', kwargs={'pk':company_id})
+        self.company = Company.objects.get(pk=company_id)
         return super(UserCreateView, self).post(request, **kwargs)
+
+    def form_valid(self, form):
+        form.instance.company = self.company
+        return super().form_valid(form)
 
 class UserUpdateView(BSModalUpdateView):
     model = User
+    form_class = UserForm
     template_name = 'user/update_user.html'
-    form_class = UserChangeForm
     success_message = 'Success: User was updated.'
-    success_url = reverse_lazy('company_detail')
+
+    def post(self, request, **kwargs):
+        self.success_url = reverse('company_detail', kwargs={'pk':self.kwargs['company_id']})
+        return super(UserUpdateView, self).post(request, **kwargs)
+
+
+class UserDeleteView(BSModalDeleteView):
+    model = User
+    template_name = 'user/delete_user.html'
+    success_message = 'Success: Book was deleted.'
+
+    def post(self, request, **kwargs):
+        self.success_url = reverse('company_detail', kwargs={'pk':self.kwargs['company_id']})
+        return super(UserDeleteView, self).post(request, **kwargs)
+
+'''For printing post requests if needed. '''
+def pretty_request(request):
+    headers = ''
+    for header, value in request.META.items():
+        if not header.startswith('HTTP'):
+            continue
+        header = '-'.join([h.capitalize() for h in header[5:].lower().split('_')])
+        headers += '{}: {}\n'.format(header, value)
+
+    return (
+        '{method} HTTP/1.1\n'
+        'Content-Length: {content_length}\n'
+        'Content-Type: {content_type}\n'
+        '{headers}\n\n'
+        '{body}'
+    ).format(
+        method=request.method,
+        content_length=request.META['CONTENT_LENGTH'],
+        content_type=request.META['CONTENT_TYPE'],
+        headers=headers,
+        body=request.body,
+    )
 
